c036fc2a4c70377d4bcb7c2435d810390fc72524
Jerry Leung <jerrytfleung@users.noreply.github.com>
2024-05-16 15:53:52 -0700

    NH-76996 Otelcol: collecting Lambda function logs (#18)
    * Added logs receiver
    * Added basic test cases
    * Decoupled API version from schema version
    * Fixed the data type of Record for parsing different type of logs
    * Updated to use semconv v1.25.0
    * Added SubscribeEvents function so as to accept events parameter to subscribe

--- collector/internal/telemetryapi/client.go
+++ collector/internal/telemetryapi/client.go
@@ -49,13 +49,7 @@ func NewClient(logger *zap.Logger) *Client {
 	}
 }
 
-func (c *Client) Subscribe(ctx context.Context, extensionID string, listenerURI string) (string, error) {
-	eventTypes := []EventType{
-		Platform,
-		// Function,
-		// Extension,
-	}
-
+func (c *Client) SubscribeEvents(ctx context.Context, eventTypes []EventType, extensionID string, listenerURI string) (string, error) {
 	bufferingConfig := BufferingCfg{
 		MaxItems:  1000,
 		MaxBytes:  256 * 1024,
@@ -108,6 +102,15 @@ func (c *Client) Subscribe(ctx context.Context, extensionID string, listenerURI 
 	c.logger.Info("Subscription success", zap.String("response", string(body)))
 
 	return string(body), nil
+}
+
+func (c *Client) Subscribe(ctx context.Context, extensionID string, listenerURI string) (string, error) {
+	eventTypes := []EventType{
+		Platform,
+		// Function,
+		// Extension,
+	}
+	return c.SubscribeEvents(ctx, eventTypes, extensionID, listenerURI)
 }
 
 func httpPutWithHeaders(ctx context.Context, client *http.Client, url string, data []byte, headers map[string]string) (*http.Response, error) {
