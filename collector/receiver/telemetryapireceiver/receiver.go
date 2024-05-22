373478200abf7fb683627a10258b3ad516d91cac
Jerry Leung <tingfung.leung@solarwinds.com>
2024-05-22 14:44:02 -0700

    Initial

--- collector/receiver/telemetryapireceiver/receiver.go
+++ collector/receiver/telemetryapireceiver/receiver.go
@@ -20,32 +20,37 @@ import (
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
-	"io"
-	"math/rand"
-	"net/http"
-	"os"
-	"time"
-
 	"github.com/golang-collections/go-datastructures/queue"
+	"github.com/open-telemetry/opentelemetry-lambda/collector/internal/telemetryapi"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/receiver"
 	semconv "go.opentelemetry.io/collector/semconv/v1.25.0"
 	"go.uber.org/zap"
-
-	"github.com/open-telemetry/opentelemetry-lambda/collector/internal/telemetryapi"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 const defaultListenerPort = "4325"
 const initialQueueSize = 5
+const timeFormatLayout = "2006-01-02T15:04:05.000Z"
+const scopeName = "github.com/open-telemetry/opentelemetry-lambda/collector/receiver/telemetryapi"
 
 type telemetryAPIReceiver struct {
 	httpServer            *http.Server
 	logger                *zap.Logger
 	queue                 *queue.Queue // queue is a synchronous queue and is used to put the received log events to be dispatched later
-	nextConsumer          consumer.Traces
+	nextTraces            consumer.Traces
+	nextMetrics           consumer.Metrics
+	nextLogs              consumer.Logs
 	lastPlatformStartTime string
 	lastPlatformEndTime   string
 	extensionID           string
@@ -64,7 +69,7 @@ func (r *telemetryAPIReceiver) Start(ctx context.Context, host component.Host) e
 	}()
 
 	telemetryClient := telemetryapi.NewClient(r.logger)
-	_, err := telemetryClient.Subscribe(ctx, r.extensionID, fmt.Sprintf("http://%s/", address))
+	_, err := telemetryClient.SubscribeEvents(ctx, []telemetryapi.EventType{telemetryapi.Platform, telemetryapi.Function}, r.extensionID, fmt.Sprintf("http://%s/", address))
 	if err != nil {
 		r.logger.Info("Listening for requests", zap.String("address", address), zap.String("extensionID", r.extensionID))
 		return err
@@ -113,6 +118,26 @@ func (r *telemetryAPIReceiver) httpHandler(w http.ResponseWriter, req *http.Requ
 		return
 	}
 
+	// traces
+	r.processTraces(slice)
+
+	// metrics
+	r.processMetrics(slice)
+
+	// Logs
+	if logs, err := r.createLogs(slice); err == nil {
+		err := r.nextLogs.ConsumeLogs(context.Background(), logs)
+		if err != nil {
+			r.logger.Error("error receiving logs", zap.Error(err))
+		}
+	}
+
+	r.logger.Debug("logEvents received", zap.Int("count", len(slice)), zap.Int64("queue_length", r.queue.Len()))
+	slice = nil
+}
+
+func (r *telemetryAPIReceiver) processTraces(slice []event) {
+	// traces
 	for _, el := range slice {
 		r.logger.Debug(fmt.Sprintf("Event: %s", el.Type), zap.Any("event", el))
 		switch el.Type {
@@ -147,7 +172,7 @@ func (r *telemetryAPIReceiver) httpHandler(w http.ResponseWriter, req *http.Requ
 	}
 	if len(r.lastPlatformStartTime) > 0 && len(r.lastPlatformEndTime) > 0 {
 		if td, err := r.createPlatformInitSpan(r.lastPlatformStartTime, r.lastPlatformEndTime); err == nil {
-			err := r.nextConsumer.ConsumeTraces(context.Background(), td)
+			err := r.nextTraces.ConsumeTraces(context.Background(), td)
 			if err == nil {
 				r.lastPlatformEndTime = ""
 				r.lastPlatformStartTime = ""
@@ -156,31 +181,181 @@ func (r *telemetryAPIReceiver) httpHandler(w http.ResponseWriter, req *http.Requ
 			}
 		}
 	}
+}
 
-	r.logger.Debug("logEvents received", zap.Int("count", len(slice)), zap.Int64("queue_length", r.queue.Len()))
-	slice = nil
+func (r *telemetryAPIReceiver) processMetrics(slice []event) {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	r.resource.CopyTo(rm.Resource())
+	sm := rm.ScopeMetrics().AppendEmpty()
+
+	for _, el := range slice {
+		timestamp, err := time.Parse(timeFormatLayout, el.Time)
+		if err != nil {
+			r.logger.Error("error parsing time", zap.Error(err))
+			return
+		}
+		switch el.Type {
+		// A report of function initialization.
+		case "platform.initReport":
+			if record, ok := el.Record.(map[string]interface{}); ok {
+				if m, ok := record["metrics"]; ok {
+					if me, ok := m.(map[string]interface{}); ok {
+						if v, ok := me["durationMs"]; ok {
+							if val, ok := v.(float64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.initReport.durationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetDoubleValue(val)
+							}
+						}
+					}
+				}
+			}
+		// The runtime finished processing an event with either success or failure.
+		case "platform.runtimeDone":
+			if record, ok := el.Record.(map[string]interface{}); ok {
+				if m, ok := record["metrics"]; ok {
+					if me, ok := m.(map[string]interface{}); ok {
+						if v, ok := me["durationMs"]; ok {
+							if val, ok := v.(float64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.runtimeDone.durationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetDoubleValue(val)
+							}
+						}
+						if v, ok := me["producedBytes"]; ok {
+							if val, ok := v.(int64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.runtimeDone.producedBytes")
+								metric.SetUnit("byte")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetIntValue(val)
+							}
+						}
+					}
+				}
+			}
+		// A report of function invocation.
+		case "platform.report":
+			if record, ok := el.Record.(map[string]interface{}); ok {
+				if m, ok := record["metrics"]; ok {
+					if me, ok := m.(map[string]interface{}); ok {
+						if v, ok := me["billedDurationMs"]; ok {
+							if val, ok := v.(int64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.report.billedDurationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetIntValue(val)
+							}
+						}
+						if v, ok := me["durationMs"]; ok {
+							if val, ok := v.(float64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.report.durationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetDoubleValue(val)
+							}
+						}
+						if v, ok := me["initDurationMs"]; ok {
+							if val, ok := v.(float64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.report.initDurationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetDoubleValue(val)
+							}
+						}
+						if v, ok := me["maxMemoryUsedMB"]; ok {
+							if val, ok := v.(int64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.report.maxMemoryUsedMB")
+								metric.SetUnit("MB")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetIntValue(val)
+							}
+						}
+						if v, ok := me["memorySizeMB"]; ok {
+							if val, ok := v.(int64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.report.memorySizeMB")
+								metric.SetUnit("MB")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetIntValue(val)
+							}
+						}
+						if v, ok := me["restoreDurationMs"]; ok {
+							if val, ok := v.(float64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.report.restoreDurationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetDoubleValue(val)
+							}
+						}
+					}
+				}
+			}
+		// Report of runtime restore (reserved for future use)
+		case "platform.restoreReport":
+			if record, ok := el.Record.(map[string]interface{}); ok {
+				if m, ok := record["metrics"]; ok {
+					if me, ok := m.(map[string]interface{}); ok {
+						if v, ok := me["durationMs"]; ok {
+							if val, ok := v.(float64); ok {
+								metric := sm.Metrics().AppendEmpty()
+								metric.SetName("sw.apm.lambda.restoreReport.durationMs")
+								metric.SetUnit("ms")
+								dp := metric.SetEmptyGauge().DataPoints().AppendEmpty()
+								dp.SetTimestamp(pcommon.NewTimestampFromTime(timestamp))
+								dp.SetDoubleValue(val)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if metrics.MetricCount() > 0 {
+		if err := r.nextMetrics.ConsumeMetrics(context.Background(), metrics); err != nil {
+			r.logger.Error("error receiving metrics", zap.Error(err))
+		}
+	}
 }
 
 func (r *telemetryAPIReceiver) createPlatformInitSpan(start, end string) (ptrace.Traces, error) {
 	traceData := ptrace.NewTraces()
 	rs := traceData.ResourceSpans().AppendEmpty()
 	r.resource.CopyTo(rs.Resource())
-
 	ss := rs.ScopeSpans().AppendEmpty()
-	ss.Scope().SetName("github.com/open-telemetry/opentelemetry-lambda/collector/receiver/telemetryapi")
+	ss.Scope().SetName(scopeName)
 	span := ss.Spans().AppendEmpty()
 	span.SetTraceID(newTraceID())
 	span.SetSpanID(newSpanID())
 	span.SetName("platform.initRuntimeDone")
 	span.SetKind(ptrace.SpanKindInternal)
 	span.Attributes().PutBool(semconv.AttributeFaaSColdstart, true)
-	layout := "2006-01-02T15:04:05.000Z"
-	startTime, err := time.Parse(layout, start)
+	startTime, err := time.Parse(timeFormatLayout, start)
 	if err != nil {
 		return ptrace.Traces{}, err
 	}
 	span.SetStartTimestamp(pcommon.NewTimestampFromTime(startTime))
-	endTime, err := time.Parse(layout, end)
+	endTime, err := time.Parse(timeFormatLayout, end)
 	if err != nil {
 		return ptrace.Traces{}, err
 	}
@@ -188,11 +363,135 @@ func (r *telemetryAPIReceiver) createPlatformInitSpan(start, end string) (ptrace
 	return traceData, nil
 }
 
+func (r *telemetryAPIReceiver) createMetrics(slice []event) (pmetric.Metrics, error) {
+	return pmetric.Metrics{}, nil
+}
+
+func (r *telemetryAPIReceiver) createLogs(slice []event) (plog.Logs, error) {
+	log := plog.NewLogs()
+	resourceLog := log.ResourceLogs().AppendEmpty()
+	r.resource.CopyTo(resourceLog.Resource())
+	scopeLog := resourceLog.ScopeLogs().AppendEmpty()
+	scopeLog.Scope().SetName(scopeName)
+	for _, el := range slice {
+		r.logger.Debug(fmt.Sprintf("Event: %s", el.Type), zap.Any("event", el))
+		logRecord := scopeLog.LogRecords().AppendEmpty()
+		logRecord.Attributes().PutStr("type", el.Type)
+		if t, err := time.Parse(timeFormatLayout, el.Time); err == nil {
+			logRecord.SetTimestamp(pcommon.NewTimestampFromTime(t))
+			logRecord.SetObservedTimestamp(pcommon.NewTimestampFromTime(time.Now()))
+		} else {
+			r.logger.Error("error parsing time", zap.Error(err))
+			return plog.Logs{}, err
+		}
+		if el.Type == string(telemetryapi.Function) || el.Type == string(telemetryapi.Extension) {
+			if record, ok := el.Record.(map[string]interface{}); ok {
+				// in JSON format https://docs.aws.amazon.com/lambda/latest/dg/telemetry-schema-reference.html#telemetry-api-function
+				if timestamp, ok := record["timestamp"].(string); ok {
+					if observedTime, err := time.Parse(timeFormatLayout, timestamp); err == nil {
+						logRecord.SetTimestamp(pcommon.NewTimestampFromTime(observedTime))
+					} else {
+						r.logger.Error("error parsing time", zap.Error(err))
+						return plog.Logs{}, err
+					}
+				}
+				if level, ok := record["level"].(string); ok {
+					level = strings.ToUpper(level)
+					logRecord.SetSeverityText(level)
+					switch level {
+					case "TRACE":
+						logRecord.SetSeverityNumber(1)
+					case "TRACE2":
+						logRecord.SetSeverityNumber(2)
+					case "TRACE3":
+						logRecord.SetSeverityNumber(3)
+					case "TRACE4":
+						logRecord.SetSeverityNumber(4)
+					case "DEBUG":
+						logRecord.SetSeverityNumber(5)
+					case "DEBUG2":
+						logRecord.SetSeverityNumber(6)
+					case "DEBUG3":
+						logRecord.SetSeverityNumber(7)
+					case "DEBUG4":
+						logRecord.SetSeverityNumber(8)
+					case "INFO":
+						logRecord.SetSeverityNumber(9)
+					case "INFO2":
+						logRecord.SetSeverityNumber(10)
+					case "INFO3":
+						logRecord.SetSeverityNumber(11)
+					case "INFO4":
+						logRecord.SetSeverityNumber(12)
+					case "WARN":
+						logRecord.SetSeverityNumber(13)
+					case "WARN2":
+						logRecord.SetSeverityNumber(14)
+					case "WARN3":
+						logRecord.SetSeverityNumber(15)
+					case "WARN4":
+						logRecord.SetSeverityNumber(16)
+					case "ERROR":
+						logRecord.SetSeverityNumber(17)
+					case "ERROR2":
+						logRecord.SetSeverityNumber(18)
+					case "ERROR3":
+						logRecord.SetSeverityNumber(19)
+					case "ERROR4":
+						logRecord.SetSeverityNumber(20)
+					case "FATAL":
+						logRecord.SetSeverityNumber(21)
+					case "FATAL2":
+						logRecord.SetSeverityNumber(22)
+					case "FATAL3":
+						logRecord.SetSeverityNumber(23)
+					case "FATAL4":
+						logRecord.SetSeverityNumber(24)
+					default:
+					}
+				}
+				if requestId, ok := record["requestId"].(string); ok {
+					logRecord.Attributes().PutStr(semconv.AttributeFaaSInvocationID, requestId)
+				}
+				if line, ok := record["message"].(string); ok {
+					logRecord.Body().SetStr(line)
+				}
+			} else {
+				// in plain text https://docs.aws.amazon.com/lambda/latest/dg/telemetry-schema-reference.html#telemetry-api-function
+				if line, ok := el.Record.(string); ok {
+					logRecord.Body().SetStr(line)
+				}
+			}
+		} else {
+			logRecord.SetSeverityText("INFO")
+			logRecord.SetSeverityNumber(9)
+			if j, err := json.Marshal(el.Record); err == nil {
+				logRecord.Body().SetStr(string(j))
+			} else {
+				r.logger.Error("error stringify record", zap.Error(err))
+				return plog.Logs{}, err
+			}
+		}
+	}
+	return log, nil
+}
+
+func (r *telemetryAPIReceiver) registerTracesConsumer(next consumer.Traces) {
+	r.nextTraces = next
+}
+
+func (r *telemetryAPIReceiver) registerMetricsConsumer(next consumer.Metrics) {
+	r.nextMetrics = next
+}
+
+func (r *telemetryAPIReceiver) registerLogsConsumer(next consumer.Logs) {
+	r.nextLogs = next
+}
+
 func newTelemetryAPIReceiver(
 	cfg *Config,
-	next consumer.Traces,
 	set receiver.CreateSettings,
-) (*telemetryAPIReceiver, error) {
+) *telemetryAPIReceiver {
 	envResourceMap := map[string]string{
 		"AWS_LAMBDA_FUNCTION_MEMORY_SIZE": semconv.AttributeFaaSMaxMemory,
 		"AWS_LAMBDA_FUNCTION_VERSION":     semconv.AttributeFaaSVersion,
@@ -213,12 +512,11 @@ func newTelemetryAPIReceiver(
 		}
 	}
 	return &telemetryAPIReceiver{
-		logger:       set.Logger,
-		queue:        queue.New(initialQueueSize),
-		nextConsumer: next,
-		extensionID:  cfg.extensionID,
-		resource:     r,
-	}, nil
+		logger:      set.Logger,
+		queue:       queue.New(initialQueueSize),
+		extensionID: cfg.extensionID,
+		resource:    r,
+	}
 }
 
 func listenOnAddress() string {
--- collector/receiver/telemetryapireceiver/receiver_test.go
+++ collector/receiver/telemetryapireceiver/receiver_test.go
@@ -4,14 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package telemetryapireceiver // import "github.com/open-telemetry/opentelemetry-lambda/collector/receiver/telemetryapireceiver"
 
 import (
@@ -19,11 +18,15 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/receiver/receivertest"
+	semconv "go.opentelemetry.io/collector/semconv/v1.25.0"
 )
 
 func TestListenOnAddress(t *testing.T) {
@@ -99,12 +102,10 @@ func TestHandler(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
 			consumer := mockConsumer{}
-			r, err := newTelemetryAPIReceiver(
+			r := newTelemetryAPIReceiver(
 				&Config{},
-				&consumer,
 				receivertest.NewNopCreateSettings(),
 			)
-			require.NoError(t, err)
 			req := httptest.NewRequest("POST",
 				"http://localhost:53612/someevent", strings.NewReader(tc.body))
 			rec := httptest.NewRecorder()
@@ -146,17 +147,271 @@ func TestCreatePlatformInitSpan(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			r, err := newTelemetryAPIReceiver(
+			r := newTelemetryAPIReceiver(
 				&Config{},
-				nil,
 				receivertest.NewNopCreateSettings(),
 			)
-			require.NoError(t, err)
 			td, err := r.createPlatformInitSpan(tc.start, tc.end)
 			if tc.expectError {
 				require.Error(t, err)
 			} else {
 				require.Equal(t, tc.expected, td.SpanCount())
+			}
+		})
+	}
+}
+
+func TestProcessMetrics(t *testing.T) {
+	testCases := []struct {
+		desc                      string
+		slice                     []event
+		expectedLogRecords        int
+		expectedType              string
+		expectedTimestamp         string
+		expectedBody              string
+		expectedSeverityText      string
+		expectedContainsRequestId bool
+		expectedRequestId         string
+		expectedSeverityNumber    plog.SeverityNumber
+		expectError               bool
+	}{
+		{
+			desc: "platform.initReport",
+			slice: []event{
+				{
+					Time: "2024-05-15T23:58:26.858Z",
+					Type: "platform.initReport",
+					Record: map[string]any{
+						"initializationType": "on-demand",
+						"metrics": map[string]any{
+							"durationMs": 1819.081,
+						},
+						"phase":  "init",
+						"status": "success",
+					},
+				},
+			},
+			expectedLogRecords:        1,
+			expectedType:              "platform.initReport",
+			expectedTimestamp:         "2024-05-15T23:58:26.858Z",
+			expectedBody:              "{\"initializationType\":\"on-demand\",\"metrics\":{\"durationMs\":1819.081},\"phase\":\"init\",\"status\":\"success\"}",
+			expectedContainsRequestId: false,
+			expectedSeverityText:      "INFO",
+			expectedSeverityNumber:    plog.SeverityNumberInfo,
+			expectError:               false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			r := newTelemetryAPIReceiver(
+				&Config{},
+				receivertest.NewNopCreateSettings(),
+			)
+			r.processMetrics(tc.slice)
+			//if tc.expectError {
+			//	require.Error(t, err)
+			//} else {
+			//	require.Equal(t, 1, log.ResourceLogs().Len())
+			//	resourceLog := log.ResourceLogs().At(0)
+			//	require.Equal(t, 1, resourceLog.ScopeLogs().Len())
+			//	scopeLog := resourceLog.ScopeLogs().At(0)
+			//	require.Equal(t, scopeName, scopeLog.Scope().Name())
+			//	require.Equal(t, tc.expectedLogRecords, scopeLog.LogRecords().Len())
+			//	if scopeLog.LogRecords().Len() > 0 {
+			//		logRecord := scopeLog.LogRecords().At(0)
+			//		attr, ok := logRecord.Attributes().Get("type")
+			//		require.True(t, ok)
+			//		require.Equal(t, tc.expectedType, attr.Str())
+			//		expectedTime, err := time.Parse(timeFormatLayout, tc.expectedTimestamp)
+			//		require.NoError(t, err)
+			//		require.Equal(t, pcommon.NewTimestampFromTime(expectedTime), logRecord.Timestamp())
+			//		requestId, ok := logRecord.Attributes().Get(semconv.AttributeFaaSInvocationID)
+			//		require.Equal(t, tc.expectedContainsRequestId, ok)
+			//		if ok {
+			//			require.Equal(t, tc.expectedRequestId, requestId.Str())
+			//		}
+			//		require.Equal(t, tc.expectedSeverityText, logRecord.SeverityText())
+			//		require.Equal(t, tc.expectedSeverityNumber, logRecord.SeverityNumber())
+			//		require.Equal(t, tc.expectedBody, logRecord.Body().Str())
+			//	}
+			//}
+		})
+	}
+}
+
+func TestCreateLogs(t *testing.T) {
+	testCases := []struct {
+		desc                      string
+		slice                     []event
+		expectedLogRecords        int
+		expectedType              string
+		expectedTimestamp         string
+		expectedBody              string
+		expectedSeverityText      string
+		expectedContainsRequestId bool
+		expectedRequestId         string
+		expectedSeverityNumber    plog.SeverityNumber
+		expectError               bool
+	}{
+		{
+			desc:               "no slice",
+			expectedLogRecords: 0,
+			expectError:        false,
+		},
+		{
+			desc: "Invalid Timestamp",
+			slice: []event{
+				{
+					Time:   "invalid",
+					Type:   "function",
+					Record: "[INFO] Hello world, I am an extension!",
+				},
+			},
+			expectError: true,
+		},
+		{
+			desc: "function text",
+			slice: []event{
+				{
+					Time:   "2022-10-12T00:03:50.000Z",
+					Type:   "function",
+					Record: "[INFO] Hello world, I am an extension!",
+				},
+			},
+			expectedLogRecords:        1,
+			expectedType:              "function",
+			expectedTimestamp:         "2022-10-12T00:03:50.000Z",
+			expectedBody:              "[INFO] Hello world, I am an extension!",
+			expectedContainsRequestId: false,
+			expectedSeverityText:      "",
+			expectedSeverityNumber:    plog.SeverityNumberUnspecified,
+			expectError:               false,
+		},
+		{
+			desc: "function json",
+			slice: []event{
+				{
+					Time: "2022-10-12T00:03:50.000Z",
+					Type: "function",
+					Record: map[string]any{
+						"timestamp": "2022-10-12T00:03:50.000Z",
+						"level":     "INFO",
+						"requestId": "79b4f56e-95b1-4643-9700-2807f4e68189",
+						"message":   "Hello world, I am a function!",
+					},
+				},
+			},
+			expectedLogRecords:        1,
+			expectedType:              "function",
+			expectedTimestamp:         "2022-10-12T00:03:50.000Z",
+			expectedBody:              "Hello world, I am a function!",
+			expectedContainsRequestId: true,
+			expectedRequestId:         "79b4f56e-95b1-4643-9700-2807f4e68189",
+			expectedSeverityText:      "INFO",
+			expectedSeverityNumber:    plog.SeverityNumberInfo,
+			expectError:               false,
+		},
+		{
+			desc: "extension text",
+			slice: []event{
+				{
+					Time:   "2022-10-12T00:03:50.000Z",
+					Type:   "extension",
+					Record: "[INFO] Hello world, I am an extension!",
+				},
+			},
+			expectedLogRecords:        1,
+			expectedType:              "extension",
+			expectedTimestamp:         "2022-10-12T00:03:50.000Z",
+			expectedBody:              "[INFO] Hello world, I am an extension!",
+			expectedContainsRequestId: false,
+			expectedSeverityText:      "",
+			expectedSeverityNumber:    plog.SeverityNumberUnspecified,
+			expectError:               false,
+		},
+		{
+			desc: "extension json",
+			slice: []event{
+				{
+					Time: "2022-10-12T00:03:50.000Z",
+					Type: "extension",
+					Record: map[string]any{
+						"timestamp": "2022-10-12T00:03:50.000Z",
+						"level":     "INFO",
+						"requestId": "79b4f56e-95b1-4643-9700-2807f4e68689",
+						"message":   "Hello world, I am an extension!",
+					},
+				},
+			},
+			expectedLogRecords:        1,
+			expectedType:              "extension",
+			expectedTimestamp:         "2022-10-12T00:03:50.000Z",
+			expectedBody:              "Hello world, I am an extension!",
+			expectedContainsRequestId: true,
+			expectedRequestId:         "79b4f56e-95b1-4643-9700-2807f4e68689",
+			expectedSeverityText:      "INFO",
+			expectedSeverityNumber:    plog.SeverityNumberInfo,
+			expectError:               false,
+		},
+		{
+			desc: "platform.initReport",
+			slice: []event{
+				{
+					Time: "2024-05-15T23:58:26.858Z",
+					Type: "platform.initReport",
+					Record: map[string]any{
+						"initializationType": "on-demand",
+						"metrics": map[string]any{
+							"durationMs": 1819.081,
+						},
+						"phase":  "init",
+						"status": "success",
+					},
+				},
+			},
+			expectedLogRecords:        1,
+			expectedType:              "platform.initReport",
+			expectedTimestamp:         "2024-05-15T23:58:26.858Z",
+			expectedBody:              "{\"initializationType\":\"on-demand\",\"metrics\":{\"durationMs\":1819.081},\"phase\":\"init\",\"status\":\"success\"}",
+			expectedContainsRequestId: false,
+			expectedSeverityText:      "INFO",
+			expectedSeverityNumber:    plog.SeverityNumberInfo,
+			expectError:               false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			r := newTelemetryAPIReceiver(
+				&Config{},
+				receivertest.NewNopCreateSettings(),
+			)
+			log, err := r.createLogs(tc.slice)
+			if tc.expectError {
+				require.Error(t, err)
+			} else {
+				require.Equal(t, 1, log.ResourceLogs().Len())
+				resourceLog := log.ResourceLogs().At(0)
+				require.Equal(t, 1, resourceLog.ScopeLogs().Len())
+				scopeLog := resourceLog.ScopeLogs().At(0)
+				require.Equal(t, scopeName, scopeLog.Scope().Name())
+				require.Equal(t, tc.expectedLogRecords, scopeLog.LogRecords().Len())
+				if scopeLog.LogRecords().Len() > 0 {
+					logRecord := scopeLog.LogRecords().At(0)
+					attr, ok := logRecord.Attributes().Get("type")
+					require.True(t, ok)
+					require.Equal(t, tc.expectedType, attr.Str())
+					expectedTime, err := time.Parse(timeFormatLayout, tc.expectedTimestamp)
+					require.NoError(t, err)
+					require.Equal(t, pcommon.NewTimestampFromTime(expectedTime), logRecord.Timestamp())
+					requestId, ok := logRecord.Attributes().Get(semconv.AttributeFaaSInvocationID)
+					require.Equal(t, tc.expectedContainsRequestId, ok)
+					if ok {
+						require.Equal(t, tc.expectedRequestId, requestId.Str())
+					}
+					require.Equal(t, tc.expectedSeverityText, logRecord.SeverityText())
+					require.Equal(t, tc.expectedSeverityNumber, logRecord.SeverityNumber())
+					require.Equal(t, tc.expectedBody, logRecord.Body().Str())
+				}
 			}
 		})
 	}
