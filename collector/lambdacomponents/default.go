72021f0154729a38f29e8c05fa61755c25c1eadd
Jerry Leung <tingfung.leung@solarwinds.com>
2025-06-19 12:17:11 -0700

    Restore

--- collector/lambdacomponents/default.go
+++ collector/lambdacomponents/default.go
@@ -1,3 +1,7 @@
+//go:build !lambdacomponents.custom
+//go:build !lambdacomponents.custom
+//go:build !lambdacomponents.custom
+
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
